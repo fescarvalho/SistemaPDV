c2f15b505620ffffffab066a2b1594bc5fd780e8
fescarvalho <fernando_scarvalho@hotmail.com>
2023-10-31 11:07:12 -0300

    Config para deploy na vercel4"

--- servidor.js
+++ servidor.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./src/routes/routes');
-
 const servidor = express();
+
+app.listen(process.env.PORT || 8080);
 servidor.use(express.json());
 servidor.use(routes);
 
