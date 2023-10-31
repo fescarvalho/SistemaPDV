04cd2c107e32b1731f833f035339923ed6513901
fescarvalho <fernando_scarvalho@hotmail.com>
2023-10-30 23:11:22 -0300

    Tentativa de envio2

--- src/utils/envioDeEmail/enviarEmail.js
+++ src/utils/envioDeEmail/enviarEmail.js
@@ -51,6 +51,14 @@ const confirmarPedido = async (clientID) => {
     subject: 'Confirmação de pedido',
     html,
   });
+
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take our messages');
+    }
+  });
   return null;
 };
 
