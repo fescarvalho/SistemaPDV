f491444409b9a7c7c95cce263b3867c3a60ba637
fescarvalho <fernando_scarvalho@hotmail.com>
2023-10-31 13:04:35 -0300

    Melhorias no cadastrar pedido

--- src/controllers/pedidos/cadastrarPedido.js
+++ src/controllers/pedidos/cadastrarPedido.js
@@ -1,38 +1,39 @@
 const knex = require('../../../conexao');
-
 const confirmarPedido = require('../../utils/envioDeEmail/enviarEmail');
 
-const cadastrarPedido = async (req, res) => {
-  const { cliente_id, observacao, pedido_produtos } = req.body;
+const calcularTotalPedido = async (pedido_produtos) => {
+  let total = 0;
 
-  try {
-    //valor total
-    let total = 0;
-    for (const pedidoProduto of pedido_produtos) {
+  await Promise.all(
+    pedido_produtos.map(async (pedidoProduto) => {
       const { produto_id, quantidade_produto } = pedidoProduto;
       const produto = await knex('produtos').where('id', produto_id).first();
       const valor = produto.valor * quantidade_produto;
       total += valor;
-    }
-    //inserindo pedido
-    await knex('pedidos').insert({
-      cliente_id,
-      observacao,
-      valor_total: total,
-    });
+    }),
+  );
 
-    //buscando id do pedido no banco
-    const pedidoBanco = await knex('pedidos')
-      .select('*')
-      .orderBy('id', 'desc')
-      .first();
+  return total;
+};
 
-    //inserindo pedido_produtos
-    for (const produtosPedido of pedido_produtos) {
+const inserirPedidoNoBanco = async (cliente_id, observacao, total) => {
+  await knex('pedidos').insert({
+    cliente_id,
+    observacao,
+    valor_total: total,
+  });
+
+  const { id } = await knex('pedidos').select('*').orderBy('id', 'desc').first();
+  return id;
+};
+
+const inserirPedidoProdutos = async (pedido_id, pedido_produtos) => {
+  await Promise.all(
+    pedido_produtos.map(async (produtosPedido) => {
       const { produto_id, quantidade_produto } = produtosPedido;
       const produto = await knex('produtos').where('id', produto_id).first();
       await knex('pedido_produtos').insert({
-        pedido_id: pedidoBanco.id,
+        pedido_id,
         produto_id,
         quantidade_produto,
         valor_produto: produto.valor,
@@ -41,10 +42,21 @@ const cadastrarPedido = async (req, res) => {
       await knex('produtos')
         .where('id', produto_id)
         .decrement('quantidade_estoque', quantidade_produto);
-    }
+    }),
+  );
+};
 
-    //Enviando Email de confirmação
+const cadastrarPedido = async (req, res) => {
+  const { cliente_id, observacao, pedido_produtos } = req.body;
 
+  try {
+    const total = await calcularTotalPedido(pedido_produtos);
+
+    const pedido_id = await inserirPedidoNoBanco(cliente_id, observacao, total);
+
+    await inserirPedidoProdutos(pedido_id, pedido_produtos);
+
+    // Enviando Email de confirmação
     await confirmarPedido(cliente_id);
 
     return res.status(200).send({ message: 'Pedido registrado com sucesso!' });
--- src/utils/envioDeEmail/enviarEmail.js
+++ src/utils/envioDeEmail/enviarEmail.js
@@ -6,48 +6,42 @@ const mail = require('../../mail/sendMailJet');
 const confirmarPedido = async (clientID) => {
   const { nome, email } = await knex('clientes').where('id', clientID).first();
 
-  const pedidoBanco = await knex('pedidos')
-    .select('*')
-    .orderBy('id', 'desc')
-    .first();
+  const pedidoBanco = await knex('pedidos').select('*').orderBy('id', 'desc').first();
+  const pedidos_produtos = await knex('pedido_produtos').where('pedido_id', pedidoBanco.id);
 
-  const pedidos_produtos = await knex('pedido_produtos').where(
-    'pedido_id',
-    pedidoBanco.id,
-  );
-  let tabelaProdutosHTML = '';
-
-  for (const {
-    produto_id,
-    quantidade_produto,
-    valor_produto,
-  } of pedidos_produtos) {
-    const { descricao } = await knex('produtos')
-      .where('id', produto_id)
-      .first();
-    tabelaProdutosHTML += `<tr>
-    <td>${descricao}</td>
-    <td>${quantidade_produto}</td>
-    <td>${(valor_produto.toFixed(2) / 100).toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
-    })}</td>
-    <td>${(
-      (quantidade_produto * valor_produto).toFixed(2) / 100
-    ).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-  </tr>`;
-  }
-
-  const html = await compiladorHTML(
-    './src/utils/envioDeEmail/templatesMail/templateEMAIL.html',
-    {
-      nomeusuario: nome,
-      tableprodutos: tabelaProdutosHTML,
-      valortotaldopedido: `${(
-        pedidoBanco.valor_total.toFixed(2) / 100
-      ).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`,
+  const tabelaProdutosPromises = pedidos_produtos.map(
+    async ({ produto_id, quantidade_produto, valor_produto }) => {
+      const { descricao } = await knex('produtos').where('id', produto_id).first();
+      const valorFormatado = (valor_produto.toFixed(2) / 100).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      });
+      const valorTotalFormatado = (
+        (quantidade_produto * valor_produto).toFixed(2) / 100
+      ).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      });
+      return `<tr>
+      <td>${descricao}</td>
+      <td>${quantidade_produto}</td>
+      <td>${valorFormatado}</td>
+      <td>${valorTotalFormatado}</td>
+    </tr>`;
     },
   );
+  const tabelaProdutosHTML = (await Promise.all(tabelaProdutosPromises)).join('');
+
+  const valorTotalFormatado = (pedidoBanco.valor_total.toFixed(2) / 100).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  const html = await compiladorHTML('./src/templatesMail/templateEmail.html', {
+    nomeusuario: nome,
+    tableprodutos: tabelaProdutosHTML,
+    valortotaldopedido: valorTotalFormatado,
+  });
 
   /*   transporter.sendMail({
     from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
