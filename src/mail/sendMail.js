ac5f1c526f44d50e48c6ad67e0e1637fd021cea1
fescarvalho <fernando_scarvalho@hotmail.com>
2023-10-30 23:06:07 -0300

    Tentativa de envio

--- src/mail/sendMail.js
+++ src/mail/sendMail.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_ROST,
   port: process.env.EMAIL_PORT,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_KEY,
