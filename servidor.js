7bdf9c8d40b0a749545cb1eba64d6f6f389d13f7
fescarvalho <fernando_scarvalho@hotmail.com>
2023-10-31 11:00:04 -0300

    Config para deploy na vercel-3"

--- servidor.js
+++ servidor.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./src/routes/routes');
 
 const servidor = express();
 servidor.use(express.json());
 servidor.use(routes);
+servidor.listen(process.env.PORT || 9001);
 
 module.exports = servidor;
